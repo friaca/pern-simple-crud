eae4a099e83d7a69c295d9730e5f4e45dcb8391d
friaca <v.fria@outlook.com>
2020-06-23 22:07:43 -0300

    [change] moving UserCard and BookCard to components folder

---
+++ frontend/src/components/UserCard/UserCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Button from "../Button/Button";
+import "./UserCard.css";
+
+function UserCard(props) {
+  const buttonStyle = {
+    width: "100%",
+    textAlign: "end",
+    paddingRight: "10px",
+    paddingTop: "10px",
+  };
+
+  return (
+    <div className="user-card">
+      <div className="user-img">
+        <img
+          src="https://via.placeholder.com/100"
+          alt="Imagem do usuário"
+        ></img>
+      </div>
+      <div className="user-info">
+        <p>
+          {props.name}, {props.age} anos
+        </p>
+        <p>{props.tel}</p>
+        <p>{props.email}</p>
+      </div>
+      <Button style={buttonStyle}>Editar</Button>
+    </div>
+  );
+}
+
+export default UserCard;
