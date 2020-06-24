c0ee423a51c18ea7bc6471b57889f0b1ddb2cf43
friaca <v.fria@outlook.com>
2020-06-24 01:12:49 -0300

    [change] BookCard from class to function

--- frontend/src/components/BookCard/BookCard.js
+++ frontend/src/components/BookCard/BookCard.js
@@ -1,16 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
+import Button from "../Button/Button";
 import "./BookCard.css";
 
-class BookCard extends Component {
-  render() {
-    return (
-      <div className="book-card">
-        <p>{this.props.title}</p>
-        <p>{this.props.year}</p>
-        <p>{this.props.author.name}</p>
+function BookCard(props) {
+  const buttonStyle = {
+    width: "100%",
+    textAlign: "end",
+    paddingRight: "10px",
+    paddingTop: "10px",
+  };
+
+  return (
+    <div className="book-card">
+      <div className="book-info">
+        <p>{props.title}</p>
+        <p>{props.year}</p>
+        <p>{props.author.name}</p>
       </div>
-    );
-  }
+      <Button style={buttonStyle}>Editar</Button>
+    </div>
+  );
 }
 
 export default BookCard;
