f57cbf5da104fc1426eac895aaf49b34783e8a50
friaca <v.fria@outlook.com>
2020-07-26 21:24:10 -0300

    [add] a lot of user stuff, basically the save feature

--- frontend/src/components/UserCard/UserCard.js
+++ frontend/src/components/UserCard/UserCard.js
@@ -1,35 +1,50 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
+import modalStyles from "../../modalStyles";
 import Button from "../Button";
-import { UserCardWrapper, UserImgWrapper, UserInfoWrapper } from "./styles";
 import ModalParts from "../ModalParts";
+import Field from "../Field";
+import { UserCardWrapper, UserImgWrapper, UserInfoWrapper } from "./styles";
 
-const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    padding: "0px",
-    marginRight: "-50%",
-    width: "50%",
-    height: "50%",
-    transform: "translate(-50%, -50%)",
-    overflowY: "hidden",
-    display: "flex",
-    flexDirection: "column",
-  },
-};
-
-function UserCard(props) {
+function UserCard({name, age, email, phone, changeHandler}) {
   const [isModalOpen, setIsOpen] = useState(false);
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
 
-  function openModal() {
-    setIsOpen(true);
+  const [user, setUser] = useState({ name, age, email, phone });
+
+  function isValidUser(user) {
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    const phoneRegex = /^\([0-9]{2}\) [0-9]{5}-[0-9]{4}$/;
+
+    if (!user.name.length || user.name.length > 99)
+      return false
+    if (user.age < 0 || user.age > 100)
+      return false;
+    if (!user.email.match(emailRegex))
+      return false;
+    if (!user.phone.match(phoneRegex))
+      return false;
+
+    return true;
   }
 
-  function closeModal() {
-    setIsOpen(false);
+  function handleChangeUser(field, event) {
+    const { value } = event.target;
+    
+    setUser(previous => ({
+        ...previous,
+        [field]: value
+    }))
+  }
+
+  function saveUser() {
+    if (isValidUser(user)) {
+      changeHandler(user);
+      closeModal();
+    } else {
+      alert('Informações inválidas!');
+    }
   }
 
   return (
@@ -39,68 +54,63 @@ function UserCard(props) {
       </UserImgWrapper>
       <UserInfoWrapper>
         <p>
-          {props.name}, {props.age} anos
+          {name}, {age} anos
         </p>
-        <p>{props.tel}</p>
-        <p>{props.email}</p>
+        <p>{phone}</p>
+        <p>{email}</p>
       </UserInfoWrapper>
-      <Button padTop padRight action={openModal}>
+      <Button padTop padRight clickAction={openModal}>
         Editar
       </Button>
+      
       {isModalOpen && (
         <Modal
           isOpen={isModalOpen}
           onRequestClose={closeModal}
-          style={customStyles}
+          style={modalStyles}
         >
           <ModalParts.Header
             title="Um título"
             onCloseClick={closeModal}
           ></ModalParts.Header>
           <ModalParts.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-            feugiat volutpat justo vitae tincidunt. Vivamus quam nisl, laoreet
-            nec ligula at, placerat interdum nibh. Nulla vulputate vestibulum
-            eros tincidunt placerat. Donec nunc dolor, pulvinar at laoreet eget,
-            rutrum ut tellus. Duis ac augue sit amet libero dignissim gravida
-            vel ut velit. Praesent iaculis iaculis mi in commodo. Donec eu lacus
-            eget neque tempus sollicitudin. In hac habitasse platea dictumst.
-            Morbi vestibulum leo non egestas dapibus. Nam condimentum, leo in
-            posuere hendrerit, arcu diam condimentum orci, convallis
-            pellentesque ex arcu quis mi. Vivamus nec dui at diam vulputate
-            efficitur cursus eget risus. Praesent lacus augue, hendrerit ac erat
-            a, condimentum lobortis diam. Etiam malesuada, nisl id fermentum
-            dictum, lectus velit luctus neque, sed tempor elit turpis in lectus.
-            Curabitur non elementum libero, in mattis lacus. Phasellus egestas
-            id nulla ut gravida. Integer volutpat mattis metus a pellentesque.
-            Sed semper ipsum quis tortor euismod blandit. Aliquam auctor felis
-            nisi. Duis lacinia sapien at sagittis pharetra. Sed tempus nibh a
-            nisi dapibus, eu euismod dui pulvinar. Aenean vestibulum posuere
-            diam quis condimentum. Ut et tincidunt dolor. Nam venenatis vel urna
-            a maximus. Nam nec ligula suscipit, finibus tortor non, hendrerit
-            felis. Aliquam sed leo rhoncus, tincidunt eros eu, molestie risus.
-            Nunc consequat interdum aliquet. Vivamus et ligula imperdiet,
-            tincidunt tellus vitae, scelerisque erat. Curabitur ac eros non dui
-            viverra efficitur. Suspendisse ut magna in turpis congue posuere.
-            Cras velit felis, consectetur at ante sit amet, pharetra ornare
-            urna. Etiam efficitur malesuada interdum. Sed accumsan malesuada
-            interdum. Vestibulum lorem leo, dictum et lectus et, volutpat
-            condimentum tortor. Sed dictum ut eros mollis dapibus. Aenean sed
-            tincidunt turpis, non viverra lectus. Duis sagittis interdum
-            fringilla. Duis consectetur enim metus, sit amet aliquam elit tempor
-            viverra. Aliquam mattis ultrices nisi non porttitor. Nam at ex quis
-            eros porttitor euismod. Praesent nibh dui, vehicula sit amet diam
-            ac, rutrum mollis orci. Phasellus dictum ante blandit semper luctus.
-            Integer porttitor, leo in volutpat pulvinar, nisi sem hendrerit
-            nulla, ac sagittis lacus enim in nibh. Morbi rutrum est odio, at
-            feugiat justo porta sit amet. Sed laoreet pharetra risus in
-            ullamcorper. Quisque nunc odio, bibendum sed suscipit at, placerat
-            ac tellus. Nam lacus nisi, dictum nec auctor id, vehicula id nisi.
-            Fusce eu est ultrices, tempor diam non, faucibus ex. Praesent
-            consectetur eros vitae eros tempus condimentum.
+            <Field
+              label="Nome"
+              inputType="text"
+              fieldName="name"
+              inputValue={user.name}
+              placeholder="João da Silva"
+              changeHandler={handleChangeUser}
+            ></Field>
+            <Field
+              label="Idade"
+              inputType="number"
+              fieldName="age"
+              inputValue={user.age}
+              placeholder="15"
+              changeHandler={handleChangeUser}
+            ></Field>
+            <Field
+              label="E-mail"
+              inputType="email"
+              fieldName="email"
+              inputValue={user.email}
+              placeholder="joao@email.com"
+              changeHandler={handleChangeUser}
+            ></Field>
+            <Field
+              label="Telefone"
+              inputType="tel"
+              fieldName="phone"
+              inputValue={user.phone}
+              placeholder="(12) 91234-5678"
+              changeHandler={handleChangeUser}
+            ></Field>
           </ModalParts.Body>
           <ModalParts.Footer>
-            <Button>Salvar</Button>
+            <Button color="green" onClick={saveUser}>
+              Salvar
+            </Button>
           </ModalParts.Footer>
         </Modal>
       )}
--- frontend/src/components/UsersContainer/UsersContainer.js
+++ frontend/src/components/UsersContainer/UsersContainer.js
@@ -15,20 +15,31 @@ function UsersContainer() {
     fetchUsers();
   }, []);
 
+  function handleUserChange(id) {
+    return function (newUser) {
+      const ogUsers = [...users];
+      const userIndex = users.findIndex((user) => user.id === id);
+
+      ogUsers[userIndex] = { ...ogUsers[userIndex], ...newUser };
+      setUsers(ogUsers);
+    };
+  }
+
   let usersDisplay = users.map((user) => (
     <UserCard
       key={user.id}
       name={user.name}
       age={user.age}
-      tel={user.phone}
+      phone={user.phone}
       email={user.email}
+      changeHandler={handleUserChange(user.id)}
     ></UserCard>
   ));
 
   return (
     <UsersContainerWrapper className="container" id="users-container">
       <h3>Usuários</h3>
-      <div class="users">{usersDisplay}</div>
+      <div className="users">{usersDisplay}</div>
     </UsersContainerWrapper>
   );
 }
