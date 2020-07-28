c54688c368de9298750a9b0b21252a2de5c9abce
friaca <v.fria@outlook.com>
2020-07-28 02:45:19 -0300

    [add] initial book modal

--- frontend/src/components/BookCard/BookCard.js
+++ frontend/src/components/BookCard/BookCard.js
@@ -1,19 +1,106 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "../Button";
+import Modal from "react-modal";
+import modalStyles from "../../modalStyles";
+import ModalParts from "../ModalParts";
+import Field from "../Field";
+import Dropdown from '../Dropdown';
 import { BookCardWrapper, BookInfoWrapper } from "./styles";
 
-function BookCard(props) {
+function BookCard({ id, title, year, author, authorsCollection }) {
+  const [isModalOpen, setIsOpen] = useState(false);
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
+  const [book, setBook] = useState({ id, title, year, author });
+  const [authorsDropdown, setAuthorsDropdown] = useState([]);
+  const [selectedAuthor, setSelectedAuthor] = useState("");
+
+  useEffect(() => {
+    setAuthorsDropdown(authorsCollection.map(formatDropdownAuthor))
+    
+    const selectedIndex = authorsCollection.findIndex(author => author.id === book.author.id);
+    setSelectedAuthor(authorsCollection[selectedIndex].id)
+  }, [])
+
+  function formatDropdownAuthor(author) {
+    return { text: author.name,  id: author.id }
+  }
+
+  function handleChangeBook(field, event) {
+    const { value } = event.target;
+    
+    setBook(previous => ({
+        ...previous,
+        [field]: value
+    }))
+  }
+
+  function onSelectOption(option) {
+    const fullAuthor = authorsCollection.find(author => author.id === Number(option.id));
+    
+    setSelectedAuthor(fullAuthor.id);
+    handleChangeBook('author', { target: { value: fullAuthor }});
+  }
+
   return (
     <BookCardWrapper>
       <BookInfoWrapper>
         <p>
-          {props.title}, {props.year}
+          {book.title}, {book.year}
         </p>
-        <p>{props.author.name}</p>
+        <p>{book.author.name}</p>
       </BookInfoWrapper>
-      <Button padTop padRight>
+      <Button padTop padRight clickAction={openModal}>
         Editar
       </Button>
+      <Button color="red" padTop padRight>
+        Deletar
+      </Button>
+      {isModalOpen && (
+        <Modal
+          isOpen={isModalOpen}
+          onRequestClose={closeModal}
+          style={modalStyles}
+          ariaHideApp={false}
+        >
+          <ModalParts.Header
+            title="Editar livro"
+            onCloseClick={closeModal}
+          >
+          </ModalParts.Header>
+          <ModalParts.Body>
+            <Dropdown
+              label="Autor"
+              options={authorsDropdown}
+              selected={selectedAuthor}
+              onSelect={onSelectOption}
+            >
+            </Dropdown>
+            <Field
+              label="Título"
+              inputType="text"
+              fieldName="title"
+              inputValue={title}
+              placeholder="(12) 91234-5678"
+              changeHandler={handleChangeBook}
+            ></Field>
+            <Field
+              label="Ano"
+              inputType="number"
+              fieldName="year"
+              inputValue={book.year}
+              placeholder="(12) 91234-5678"
+              changeHandler={handleChangeBook}
+            ></Field>
+          </ModalParts.Body>
+          <ModalParts.Footer>
+            <Button color="green">
+              Salvar
+            </Button>
+          </ModalParts.Footer>
+        </Modal>
+      )}
     </BookCardWrapper>
   );
 }
--- frontend/src/components/BooksContainer/BooksContainer.js
+++ frontend/src/components/BooksContainer/BooksContainer.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from "react";
 import BookCard from "../BookCard";
+import Button from '../Button';
+import Modal from "react-modal";
+import modalStyles from "../../modalStyles";
+import ModalParts from "../ModalParts";
+import Field from "../Field";
 import { BooksContainerWrapper } from "./styles";
 import api from "../../api";
 
 function BooksContainer() {
   const [books, setBooks] = useState([]);
+  const [authors, setAuthors] = useState([]);
+
+  const [isModalOpen, setIsOpen] = useState(false);
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
 
   useEffect(() => {
     async function fetchBooks() {
@@ -12,21 +22,32 @@ function BooksContainer() {
       setBooks(booksResponse.data);
     }
 
+    async function fetchAuthors() {
+      const authorsResponse = await api.get("/users");
+      setAuthors(authorsResponse.data);
+    }
+
+    fetchAuthors();
     fetchBooks();
   }, []);
 
   let booksDisplay = books.map((book) => (
     <BookCard
       key={book.id}
+      id={book.id}
       title={book.title}
       year={book.year}
       author={book.author}
+      authorsCollection={authors}
     ></BookCard>
   ));
 
   return (
     <BooksContainerWrapper className="container" id="books-container">
-      <h3>Livros</h3>
+      <div className="flex space-between">
+        <h3>Livros</h3>
+        <Button color="green" onClick={openModal}>Criar</Button>
+      </div>
       <div className="books">{booksDisplay}</div>
     </BooksContainerWrapper>
   );
