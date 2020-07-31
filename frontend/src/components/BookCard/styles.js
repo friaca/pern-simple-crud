f1c04d28e21f87ac323f5d3e62419684f8f4a78c
friaca <v.fria@outlook.com>
2020-07-31 20:42:55 -0300

    [change] removing line-height and changing to margin

--- frontend/src/components/BookCard/styles.js
+++ frontend/src/components/BookCard/styles.js
@@ -3,15 +3,20 @@ import styled from "styled-components";
 export const BookCardWrapper = styled.div`
   border-radius: 4px;
   margin-top: 10px;
-  line-height: 0.5em;
   background-color: #d17fff;
   display: flex;
   -webkit-box-shadow: 10px 10px 20px -10px rgba(0, 0, 0, 0.25);
   -moz-box-shadow: 10px 10px 20px -10px rgba(0, 0, 0, 0.25);
   box-shadow: 10px 10px 20px -10px rgba(0, 0, 0, 0.25);
+  min-height: 97px;
 `;
 
 export const BookInfoWrapper = styled.div`
   padding: 10px;
   flex-grow: 2;
+  margin-top: 5px;
+
+  > p {
+    margin: 10px;
+  }
 `;
--- frontend/src/components/UserCard/styles.js
+++ frontend/src/components/UserCard/styles.js
@@ -4,18 +4,17 @@ export const UserCardWrapper = styled.div`
   display: flex;
   margin-top: 10px;
   border-radius: 4px;
-  line-height: 0.7em;
   background-color: #91ccff;
   -webkit-box-shadow: 10px 10px 20px -10px rgba(0, 0, 0, 0.25);
   -moz-box-shadow: 10px 10px 20px -10px rgba(0, 0, 0, 0.25);
   box-shadow: 10px 10px 20px -10px rgba(0, 0, 0, 0.25);
 `;
 
-export const UserImgWrapper = styled.div`
-  align-self: center;
-  padding: 10px;
-`;
-
 export const UserInfoWrapper = styled.div`
   width: 100%;
+  margin-left: 17px;
+
+  > p {
+    margin: 13px;
+  }
 `;
